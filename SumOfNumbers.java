b5a59dfb80ccd34342cd07fa8a25e69bf106ae9d
sarvagna <224g1a3397@srit.ac.in>
2024-01-15 13:30:32 +0530

    committing SumOfNUmbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,8 @@
+class SumOfNumbers{
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        int c=a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+c);
+    }
+}
